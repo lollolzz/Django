7fe02a6a941244202dc7f29479205d68497eed65
lollolzz <lollolzz1018@gmail.com>
2022-04-01 21:38:50 +0900

    Django

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField()
+
+
+
+
+
+# Create your models here.
