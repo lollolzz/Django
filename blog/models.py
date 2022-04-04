b4ed4d39f361c3ae792905c84921fafac26b4caa
lollolzz <lollolzz1018@gmail.com>
2022-04-04 21:18:05 +0900

    시간 자동 설정 및 제목 설정

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,14 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    # 시간 자동 설정
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author : 추후 작석 예정
 
-
-
-
+    # title제목 설정하는 것
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+        # {}만 의미를 가지고 [], () 들은 그대로 출력된다.
 
 # Create your models here.
